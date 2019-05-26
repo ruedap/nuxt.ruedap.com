b95b6b5cc9cc35d909c1ff0c49c630a1b160dff9
ruedap <ruedap@ruedap.com>
2019-05-26 18:06:05 +0900

    feat(pages): 各ページのコンテンツを追加

--- pages/01/index.vue
+++ pages/01/index.vue
@@ -1,3 +1,22 @@
 <template lang="pug">
-  h1 vol.1
+div
+  h1 01
+  img.thumbnail(src="./assets/01.png")
+  p 今から1年ほど前に、自分でお気に入りのブログ環境を構築する記事が話題になっていて、それを読んだ時から、自分用のブログ環境を作りたいと思っていた。
+  ul
+    li
+      a(href="http://tech.nitoyon.com/ja/blog/2012/09/20/moved-completed/") 俺の最強ブログ システムが火を噴くぜ - てっく煮ブログ
+    li
+      a(href="http://webtech-walker.com/archive/2012/09/fired-myblog.html") 俺の最強ブログシステムも火を噴いてたぜ - Webtech Walker
+  p
+    span 当時はそれどころではなかったこともあって、なかなか着手できずにいたんだけど、
+    a(href="http://blog.ruedap.com/2013/07/26/reboooot") 今年の7月頃
+    span から作り始めて、最近そこそこ希望通りの形になったので、まだやり残しはたくさんあるけれど、一旦ここで一区切りということでその内容を記事にしてみる。
 </template>
+
+<style lang="scss" scoped>
+.thumbnail {
+  width: 240px;
+  height: 120px;
+}
+</style>
--- pages/02/index.vue
+++ pages/02/index.vue
@@ -1,3 +1,18 @@
 <template lang="pug">
-  h1 vol.2
+div
+  h1 02
+  img.thumbnail(src="./assets/01.png")
+
+  p 当時はブログの再開が2年ぶりだったけど、作ってみたシリーズも同じく2年ぶり。お久しぶり。さて今回は、ものすっごくニッチなツールだけど、Font Awesomeを使ってる人でAlfredを使ってる人には喜んでもらえる気がする。イコール自分のことなんですけどね。
+
+  p あのアイコンフォントのクラス名を全然覚えられなくて、アイコンの数も300個以上あって把握できなくなっていて、毎回Font Awesomeのサイトを開いて探すのも面倒なので、 Font Awesomeのアイコンをインクリメンタルサーチしてそのクラス名をペーストできるAlfred 2用のプラグインを作ってみた。
+
+  p 百聞は一見に如かず、どんなものかは以下のスクリーンキャストをご覧ください。
 </template>
+
+<style lang="scss" scoped>
+.thumbnail {
+  width: 240px;
+  height: 120px;
+}
+</style>
