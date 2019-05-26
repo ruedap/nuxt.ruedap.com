a59a6a459468ffd3df20b6048c316790e3ac104f
ruedap <ruedap@ruedap.com>
2019-05-26 19:07:03 +0900

    feat(components): ヘッダーナビのスタイルを調整

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -4,7 +4,10 @@ div
   ul
     li
       nuxt-link(to="/01") Vol.1
+    li
       nuxt-link(to="/02") Vol.2
-      //- nuxt-link(to="/03") Vol.3
-      //- nuxt-link(to="/04") Vol.4
+    //- li
+    //-   nuxt-link(to="/03") Vol.3
+    //- li
+    //-   nuxt-link(to="/04") Vol.4
 </template>
