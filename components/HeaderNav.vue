5c439a7e6b456c11c54c93c9fc62e337869a4ce2
ruedap <ruedap@ruedap.com>
2019-05-26 17:07:14 +0900

    feat(components): 2ページ分のコンポーネントを追加

---
+++ components/HeaderNav.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  ul
+    li
+      nuxt-link(to="/01") Vol.1
+      nuxt-link(to="/02") Vol.2
+      //- nuxt-link(to="/03") Vol.3
+      //- nuxt-link(to="/04") Vol.4
+</template>
